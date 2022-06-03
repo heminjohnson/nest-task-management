4aa64305d2e9152e7afbec980bc4afa3b9beb854
Hemin Johnson <achayan1992@gmail.com>
2022-06-03 13:38:13 +0200

    feat: add a task model

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,12 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TasksStatus;
+}
+
+enum TasksStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
