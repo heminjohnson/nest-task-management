40ec54d99327e78e598e9e886e08bef91dbcad4d
Hemin Johnson <achayan1992@gmail.com>
2022-06-11 21:38:25 +0200

    feat: add error handling

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TasksStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TasksStatus)
   status?: TasksStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
